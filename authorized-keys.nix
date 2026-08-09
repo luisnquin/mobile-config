5ffe78a907c83cda6dfbda37c585bc862443ae04
Luis Quiñones <lpaandres2020@gmail.com>
2026-08-09 08:40:40 -0500

    feat(session): add a second authorized key

--- authorized-keys.nix
+++ authorized-keys.nix
@@ -2,4 +2,5 @@
 # not replace this list grants its author root on the device.
 [
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIXW6vsDRgI/AiOdGnQOTyiz1uLFL0o66u0Ahcw9VWyd luis@quinones.pro"
+  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMNOYm8dmSXKjgaBQDWCnSvcsGyiJILX3Vwejmkm150+"
 ]
